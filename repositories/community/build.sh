51dc22fa3ecc92b80334a6544d0ba5e17f2e1270
mudler <mudler@sabayon.org>
2016-02-24 11:27:41 +0100

    fixing permission and setting up binhost

--- repositories/community/build.sh
+++ repositories/community/build.sh
@@ -4,11 +4,11 @@ set -e
 . /vagrant/scripts/repositories.sh
 
 # Build community repository
-sabayon-buildpackages app-text/cherrytree \
+OUTPUT_DIR="/vagrant/artifacts/community-binhost" sabayon-buildpackages app-text/cherrytree \
                       --layman and3k-sunrise
 
 # Create repository
-REPOSITORY_NAME="community" REPOSITORY_DESCRIPTION="Community Repository"  OUTPUT_DIR="/vagrant/artifacts/community" sabayon-createrepo
+REPOSITORY_NAME="community" REPOSITORY_DESCRIPTION="Community Repository" PORTAGE_ARTIFACTS="/vagrant/artifacts/community-binhost" OUTPUT_DIR="/vagrant/artifacts/community" sabayon-createrepo
 
 # Deploy repository "community" locally and remotely (if configured)
 #deploy_all "community"
--- repositories/pantheon/build.sh
+++ repositories/pantheon/build.sh
@@ -4,10 +4,10 @@ set -e
 . /vagrant/scripts/repositories.sh
 
 # Build Pantheon repository
-sabayon-buildpackages pantheon-base/pantheon-shell --layman elementary
+OUTPUT_DIR="/vagrant/artifacts/pantheon-binhost"  sabayon-buildpackages pantheon-base/pantheon-shell --layman elementary
 
 # Create repository
-REPOSITORY_NAME="pantheon" REPOSITORY_DESCRIPTION="Pantheon Desktop Environment Community Repository"  OUTPUT_DIR="/vagrant/artifacts/pantheon" sabayon-createrepo
+REPOSITORY_NAME="pantheon" REPOSITORY_DESCRIPTION="Pantheon Desktop Environment Community Repository" PORTAGE_ARTIFACTS="/vagrant/artifacts/pantheon-binhost" OUTPUT_DIR="/vagrant/artifacts/pantheon" sabayon-createrepo
 
 # Deploy repository "pantheon" inside "repositories"
 #deploy_all "pantheon"
