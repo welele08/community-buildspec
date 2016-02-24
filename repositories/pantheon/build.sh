60a82c60daca1d1565edd41b0e0db3be9b7e345c
mudler <mudler@sabayon.org>
2016-02-24 15:07:07 +0100

    cleanup

--- repositories/pantheon/build.sh
+++ repositories/pantheon/build.sh
@@ -4,10 +4,12 @@ set -e
 . /vagrant/scripts/repositories.sh
 
 # Build Pantheon repository
-OUTPUT_DIR="/vagrant/artifacts/pantheon-binhost"  sabayon-buildpackages pantheon-base/pantheon-shell --layman elementary
+DOCKER_PULL_IMAGE=1 OUTPUT_DIR="/vagrant/artifacts/pantheon-binhost"  sabayon-buildpackages pantheon-base/pantheon-shell \
+                                                                                            --layman elementary \
+                                                                                            --equo x11-libs/gtk+:3
 
 # Create repository
-REPOSITORY_NAME="pantheon" REPOSITORY_DESCRIPTION="Pantheon Desktop Environment Community Repository" PORTAGE_ARTIFACTS="/vagrant/artifacts/pantheon-binhost" OUTPUT_DIR="/vagrant/artifacts/pantheon" sabayon-createrepo
+DOCKER_PULL_IMAGE=1 REPOSITORY_NAME="pantheon" REPOSITORY_DESCRIPTION="Pantheon Desktop Environment Community Repository" PORTAGE_ARTIFACTS="/vagrant/artifacts/pantheon-binhost" OUTPUT_DIR="/vagrant/artifacts/pantheon" sabayon-createrepo
 
 # Deploy repository "pantheon" inside "repositories"
 #deploy_all "pantheon"
