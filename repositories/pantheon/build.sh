68c6ba53246dc94cb4603f19fd6e14662bd9eb1d
mudler <mudler@sabayon.org>
2016-02-26 23:02:51 +0100

    [repositories] default for DOCKER_PULL_IMAGE now can be overrided by env variables when called directly

--- repositories/pantheon/build.sh
+++ repositories/pantheon/build.sh
@@ -3,16 +3,25 @@ set -e
 
 . /vagrant/scripts/repositories.sh
 
-export DOCKER_PULL_IMAGE=1
+export DOCKER_PULL_IMAGE="${DOCKER_PULL_IMAGE:-1}"
 export EMERGE_DEFAULTS_ARGS="-k --accept-properties=-interactive --verbose --oneshot --complete-graph --buildpkg"
 export REPOSITORY_NAME="pantheon"
 export REPOSITORY_DESCRIPTION="Pantheon Desktop Environment Community Repository"
 
-BUILD_ARGS=(
-      "pantheon-base/pantheon-shell"
-      "--layman elementary"
-      "--install x11-libs/gtk+:3"
-      "--remove dev-libs/libdbusmenu"
- )
+# Build Pantheon repository
+OUTPUT_DIR="/vagrant/artifacts/pantheon-binhost" sabayon-buildpackages \
+                                                                        pantheon-base/pantheon-shell \
+                                                                        --layman elementary \
+                                                                        --equo x11-libs/gtk+:3
 
-build_all "${BUILD_ARGS[@]}"
+# Creating our permanent binhost
+cp -rf "/vagrant/artifacts/pantheon-binhost/*" $TEMPDIR
+
+# Create repository
+PORTAGE_ARTIFACTS="/vagrant/artifacts/pantheon-binhost" OUTPUT_DIR="/vagrant/artifacts/pantheon" sabayon-createrepo
+
+
+rm -rf $TEMPDIR/*
+
+# Deploy repository "pantheon" inside "repositories"
+#deploy_all "pantheon"
