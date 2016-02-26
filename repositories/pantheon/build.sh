f6cc138ac8a2733a51bcac2155a787b833ba543e
mudler <mudler@sabayon.org>
2016-02-26 23:04:36 +0100

    [repositories/pantheon] fixing compilation

--- repositories/pantheon/build.sh
+++ repositories/pantheon/build.sh
@@ -8,20 +8,11 @@ export EMERGE_DEFAULTS_ARGS="-k --accept-properties=-interactive --verbose --one
 export REPOSITORY_NAME="pantheon"
 export REPOSITORY_DESCRIPTION="Pantheon Desktop Environment Community Repository"
 
-# Build Pantheon repository
-OUTPUT_DIR="/vagrant/artifacts/pantheon-binhost" sabayon-buildpackages \
-                                                                        pantheon-base/pantheon-shell \
-                                                                        --layman elementary \
-                                                                        --equo x11-libs/gtk+:3
+BUILD_ARGS=(
+      "pantheon-base/pantheon-shell"
+      "--layman elementary"
+      "--install x11-libs/gtk+:3"
+      "--remove dev-libs/libdbusmenu"
+ )
 
-# Creating our permanent binhost
-cp -rf "/vagrant/artifacts/pantheon-binhost/*" $TEMPDIR
-
-# Create repository
-PORTAGE_ARTIFACTS="/vagrant/artifacts/pantheon-binhost" OUTPUT_DIR="/vagrant/artifacts/pantheon" sabayon-createrepo
-
-
-rm -rf $TEMPDIR/*
-
-# Deploy repository "pantheon" inside "repositories"
-#deploy_all "pantheon"
+build_all "${BUILD_ARGS[@]}"
