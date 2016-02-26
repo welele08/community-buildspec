a99162c3d54b3a14fd7a63f9f6c029e44d240331
mudler <mudler@sabayon.org>
2016-02-26 23:13:35 +0100

    [script/repositories.sh] REPOSITORY_NAME, otherwise specified in build.sh, now defaults to repository directory name

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -13,13 +13,5 @@ update_vagrant_repo
 
 for i in "${REPOSITORIES[@]}"
 do
-	pushd /vagrant/repositories/$i
-  ### XXX: Libchecks in there!
-      send_email "[$i] $NOW Build" "Build started for $i at $NOW, temp log is on $TEMPLOG"
-			[ -f "build.sh" ] && ./build.sh  1>&2 > $TEMPLOG
-      mytime=$(date +%s)
-      ansifilter $TEMPLOG > "/vagrant/logs/$NOW/$i.$mytime.log"
-      chmod 444 /vagrant/logs/$NOW/$i.$mytime.log
-			send_email "[$i] $NOW Build" "Finished, log is available at: /vagrant/logs/$NOW/$i.$mytime.log"
-	popd
+  automated_build $i
 done
