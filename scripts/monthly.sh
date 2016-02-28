ce1841c4fbc0e3fad28b0e47001239a0068a7a57
mudler <mudler@sabayon.org>
2016-02-28 16:38:58 +0100

    VAGRANT_DIR holds the repository location on the machine who execute scripts now, cleaning DISTFILES and entropy cache monthly

--- scripts/monthly.sh
+++ scripts/monthly.sh
@@ -8,12 +8,12 @@ system_upgrade
 
 for i in "${REPOSITORIES[@]}"
 do
-  pushd /vagrant/repositories/$i
+  pushd ${VAGRANT_DIR}/repositories/$i
   send_email "[$i] Cleanup" "Started clean for $i $NOW , temp log is on $TEMPLOG"
   [ -f "clean.sh" ] && ./clean.sh  1>&2 > $TEMPLOG
   mytime=$(date +%s)
   cp -rfv $TEMPLOG "/vagrant/logs/$NOW/$i-clean.$mytime.log"
-  chmod 444 /vagrant/logs/$NOW/$i-clean.$mytime.log
+  chmod 444 ${VAGRANT_DIR}/logs/$NOW/$i-clean.$mytime.log
   send_email "[$i] Cleanup" "Finished, Log is available at: /vagrant/logs/$NOW/$i-clean.$mytime.log"
   popd
 done
@@ -27,4 +27,7 @@ rm -rfv /var/lib/docker
 systemctl start docker
 
 # update crontab
-crontab /vagrant/confs/crontab
+crontab ${VAGRANT_DIR}/confs/crontab
+
+rm -rf ${DISTFILES}/*
+rm -rf ${ENTROPY_DOWNLOADED_PACKAGES}/*
--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -7,8 +7,8 @@ export DEPLOY_SERVER
 export DEPLOY_PORT
 export DOCKER_PULL_IMAGE=1
 
-mkdir -p /vagrant/logs/$NOW
-chmod -R 444 /vagrant/logs/$NOW
+mkdir -p ${VAGRANT_DIR}/logs/$NOW
+chmod -R 444 ${VAGRANT_DIR}/logs/$NOW
 update_vagrant_repo
 
 for i in "${REPOSITORIES[@]}"
