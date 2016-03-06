c8983b835e6ab18a0544bb7560ffbf7a658360e4
mudler <mudler@sabayon.org>
2016-03-06 13:46:24 +0100

    [community] adding megaglest

--- repositories/community/build.sh
+++ repositories/community/build.sh
@@ -6,9 +6,11 @@ export REPOSITORY_DESCRIPTION="Community Repository"
 
 BUILD_ARGS=(
   "app-text/cherrytree::and3k-sunrise"
+  "games-strategy/megaglest"
   "app-emulation/shashlik-bin"
   "--layman and3k-sunrise"
   "--layman anyc"
+  "--layman games-overlay"
 )
 
 build_all "${BUILD_ARGS[@]}"
