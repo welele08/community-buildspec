037be1ebca21f8932ca21135c7f1e80ff033958b
Ettore Di Giacinto <mudler@users.noreply.github.com>
2016-04-17 02:12:15 +0200

    [provision_hd] increase size

--- scripts/provision_hd.sh
+++ scripts/provision_hd.sh
@@ -11,8 +11,8 @@ fi
 dd if=/dev/zero of=/dev/sdb bs=512 count=1 conv=notrunc
 sudo pvcreate /dev/sdb
 vgcreate vg-docker /dev/sdb
-lvcreate -n datapool -L 190G vg-docker
-lvcreate -n metapool -L 50 vg-docker
+lvcreate -n datapool -L 350G vg-docker
+lvcreate -n metapool -L 149 vg-docker
 
 lvconvert -y --zero n --thinpool vg-docker/datapool --poolmetadata vg-docker/metapool
 
